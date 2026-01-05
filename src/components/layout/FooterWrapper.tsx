1e9d74d6f940e508b83a0da9f702c20680fac9a6
qq1304662219 <qq1304662219@gmail.com>
2026-01-05 23:37:20 +0800

    Fix footer visibility: Hide on homepage, show on others

--- src/components/layout/FooterWrapper.tsx
+++ src/components/layout/FooterWrapper.tsx
@@ -6,5 +6,10 @@ import { Footer } from "@/components/landing/Footer"
 export function FooterWrapper() {
   const pathname = usePathname()
   
+  // Don't show footer on homepage
+  if (pathname === "/") {
+    return null
+  }
+
   return <Footer />
 }
